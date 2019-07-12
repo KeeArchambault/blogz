fdfd64d65022544aba678e2aed05b0d9d58e2bfc
Kee Archambault <keeandraa@gmail.com>
2019-07-12 17:42:25 -0500

    added database interaction

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
+    header = db.Column(db.String(255))
     body = db.Column(db.String(255))
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, header, body):
+        self.header = header
+        self.body = body
+
 
 
 
@@ -23,17 +25,21 @@ posts = []
 def newpost():
 
     if request.method == 'POST':
-        post = request.form['post']
-        posts.append(post)
+        header = request.form['header']
+        body = request.form['body']
+        new_post = Blog(header, body)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect("/blog")
 
-    return render_template('index.html',title="New Post", posts=posts)
+    return render_template('newpost.html',title="New Post")
 
 @app.route("/blog")
 def blog(): 
 
-    return render_template("blog.html", title="Blog")
+    posts = Blog.query.all()
 
-
+    return render_template("blog.html", title="Blog", posts=posts)
 
 
 
