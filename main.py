1945d26c30761f8ab5ea0d3e7988d1d067247667
Kee Archambault <keeandraa@gmail.com>
2019-07-14 15:06:52 -0500

    started post use-cases 1 & 2

--- main.py
+++ main.py
@@ -17,29 +17,54 @@ class Blog(db.Model):
         self.body = body
 
 
-
-
-posts = []
-
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
 
+    header = ""
+    body = ""
+    body_error = ""
+    header_error = ""
+   
     if request.method == 'POST':
         header = request.form['header']
         body = request.form['body']
-        new_post = Blog(header, body)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect("/blog")
 
+        if not header:
+            header_error = "Please enter a valid title."
+            
+        if not body:
+            body_error = "Please write a post."
+
+        if not header_error and not body_error:
+            new_post = Blog(header, body)
+            db.session.add(new_post)
+            db.session.commit()
+            
+            return render_template("/case2")
+
+        return render_template('newpost.html',title="New Post", header = header, body = body, body_error = body_error, header_error = header_error)  
+    
     return render_template('newpost.html',title="New Post")
+
+@app.route("/case1")
+def case1():
+
+@app.route("/case2")
+def case2():    
+    ident = Blog.query.filter_by(header=header).id()
+    post = Blog.query.filter_by(id = ident).body()
+
+    if
+
+    return render_template("/indiv2.html", id = ident)
+
 
 @app.route("/blog")
 def blog(): 
 
     posts = Blog.query.all()
 
-    return render_template("blog.html", title="Blog", posts=posts)
+    return render_template("blog.html", title="All Posts", posts=posts)
 
 
 
